be1cb0dcb5e05fbb1e60648323d448305a6a66c8
Juan Pablo Yeverino Morales <jyeverinomor@svb.com>
2020-09-08 19:38:13 -0700

    Crear funcion jugar para mejorar readability

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -4,21 +4,22 @@ import random
 def run():
     jugar = True
     numero_aleatorio = random.randint(1, 100)
-    scores = []
     while jugar:
-        numero_elegido = int(input('Elige un numero del 1 al 100: '))
-        intento = 0
-        while numero_elegido != numero_aleatorio:
-            if numero_elegido < numero_aleatorio:
-                numero_elegido = int(input('Elige un número más grande: '))
-            else:
-                numero_elegido = int(input('Elige un número más pequeño: '))
-        intento += 1  
-        intento = str(intento)
-        print('¡Ganaste! en ' + intento + ' intentos')
+        juego(numero_aleatorio)
         jugar = keep()
 
-    scores.append(intento)
+def juego(numero_aleatorio):
+    numero_elegido = int(input('Elige un numero del 1 al 100: '))
+    intento = 0
+    while numero_elegido != numero_aleatorio:
+        if numero_elegido < numero_aleatorio:
+            numero_elegido = int(input('Elige un número más grande: '))
+        else:
+            numero_elegido = int(input('Elige un número más pequeño: '))
+    intento += 1  
+    intento = str(intento)
+    print('¡Ganaste! en ' + intento + ' intentos')
+    
 
 def keep():
     while True:
