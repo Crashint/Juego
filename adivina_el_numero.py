d8c38d052812348e8d8c1f7b190d9b08590dc227
Crashint <crashint@hotmail.com>
2020-09-10 13:47:42 -0500

    Cambios en la colocacion de 'intento', y en en la de 'option'

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,19 +1,20 @@
 import random
 
-
+#cspell: disable */
 def run():
     jugar = True
-    numero_aleatorio = random.randint(1, 100)
     scores = []
+    
     while jugar:
-        numero_elegido = int(input('Elige un numero del 1 al 100: '))
         intento = 0
+        numero_elegido = int(input('Elige un numero del 1 al 100: '))
+        numero_aleatorio = random.randint(1, 100)
         while numero_elegido != numero_aleatorio:
             if numero_elegido < numero_aleatorio:
                 numero_elegido = int(input('Elige un número más grande: '))
             else:
                 numero_elegido = int(input('Elige un número más pequeño: '))
-        intento += 1  
+            intento += 1  
         intento = str(intento)
         print('¡Ganaste! en ' + intento + ' intentos')
         jugar = keep()
@@ -22,7 +23,7 @@ def run():
 
 def keep():
     option = str(input('Deseas seguir jugando? (S/N): '))
-    if(option == "Y"):
+    if(option == "S"):
         return True
     elif(option == "N"):
         return False
